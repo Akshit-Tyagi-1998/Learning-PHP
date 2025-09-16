8392579827eda421943c246c8b38a099797a64f4
Akshit Tyagi <tyagiakshit94729@gmail.com>
2025-09-16 10:51:36 +0530

    content added in echovsprint file

--- echovsprint.php
+++ echovsprint.php
@@ -1,3 +1,14 @@
 <?php
-  
-?>
+echo ("hello");
+echo ("<br/>--------------<br/>");
+
+$num = print('hello');
+echo ("<br/>--------------<br/>");
+echo ($num);
+
+
+// We can not print an with echo and print function.
+$arr = array('raju', 'rita', 'gita', 'sita');
+echo ("<br/>--------------<br/>");
+print($arr);
+echo ($arr);
