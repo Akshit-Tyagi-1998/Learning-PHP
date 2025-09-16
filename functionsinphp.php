c0a092ed33e1d4c234768473915bb774d0332a24
Akshit Tyagi <tyagiakshit94729@gmail.com>
2025-09-16 20:02:13 +0530

    more content added in functionsinphp file

--- functionsinphp.php
+++ functionsinphp.php
@@ -1,7 +1,29 @@
 <?php
-  
-  function fun1(){
-    echo("This is function one call");
-  }
 
-?>
+function fun1()
+{
+  echo ("This is function one call");
+}
+
+fun1();
+
+print("<br/>---------------------------------<br/>");
+
+function fun2(&$var)
+{
+  $var += $var;
+  echo ($var);
+}
+
+$var = 10;
+fun2($var);
+print("<br/>---------------------------------<br/>");
+echo ($var);
+
+print("<br/>---------------------------------<br/>");
+
+$myfun = function () {
+  echo ("This is variable function");
+};
+
+$myfun();
