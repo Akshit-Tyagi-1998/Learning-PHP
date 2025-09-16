7321b1a1a0193d6c7f9d66bf0a664ba015e7db45
Akshit Tyagi <tyagiakshit94729@gmail.com>
2025-09-16 10:41:37 +0530

    Variable file created

---
+++ variablesinphp.php
@@ -0,0 +1,12 @@
+<?php
+
+// To create variable in php we uses the $ sign.
+
+$num1 = 10;
+echo ($num1 . "<br/>");
+echo ("it is a variable");
+
+// constant variable in php. These are bydefault global.
+
+define('num2', 20);
+echo ("<br/>" . num2);
